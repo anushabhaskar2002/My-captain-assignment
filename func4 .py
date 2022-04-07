1119f427512a78633badda83ab7f9b2d5f2b4047
anushabhaskar2002 <101281095+anushabhaskar2002@users.noreply.github.com>
2022-04-07 23:57:37 +0530

    Add files via upload

---
+++ func4 .py
@@ -0,0 +1,8 @@
+most_frequency = str(input())
+all_freq = {}
+for i in most_frequent:
+    if i in all_freq:
+        all_freq[i] +=1
+    else:
+        all_freq[i] = 1
+print("count of all characters is :/n"+ str(all-freq))
