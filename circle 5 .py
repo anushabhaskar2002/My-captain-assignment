8a1e0a09f1dd21fc2d8a3223b4c9b3e34c152bbb
anushabhaskar2002 <101281095+anushabhaskar2002@users.noreply.github.com>
2022-04-09 16:19:48 +0530

    Add files via upload

---
+++ circle 5 .py
@@ -0,0 +1,9 @@
+r=float(input("input the radius of the circle:"))
+area=r*r*3.14
+print(f"the area of the circle with the radius {r}:",area)
+
+
+
+fn=input ("enter filename: ")
+f = fn.split(".")
+print ("extension of the file is : " +f[-1])
